d16b6dcb0cde01fc68095c7f6dff73f749c1fa42
Jian <jian53464@gmail.com>
2025-04-09 16:07:20 +0800

    My first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins is HARD!");
+        System.out.println("Superman is not Ultraman!");
     }
 
     public String getMessage() {
