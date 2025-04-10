3020a883ec86510f2200e48a067ee5a181a819f4
Jian <jian53464@gmail.com>
2025-04-10 09:56:57 +0800

    New changes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Superman is super!");
+        System.out.println("Superman is not okay!");
     }
 
     public String getMessage() {
