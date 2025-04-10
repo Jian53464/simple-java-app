5c8ca1abd21c9c232edd79186b92c1d2c7b5726c
Jian <jian53464@gmail.com>
2025-04-10 14:15:05 +0800

    Pipeline mvn Test 2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Superman is not okay!");
+        System.out.println("I am cooked!");
     }
 
     public String getMessage() {
