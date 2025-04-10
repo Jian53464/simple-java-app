312cfe743ceb245bc4c47900246a1b4c7f684aaf
Jian <jian53464@gmail.com>
2025-04-10 15:00:53 +0800

    pipeline trigger test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("I am cooked!");
+        System.out.println("I am not cooked!");
     }
 
     public String getMessage() {
